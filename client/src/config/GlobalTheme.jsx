5a26e08a91cf2bfdb5eec4c7cc898b5dd9801464
Vadim Gringauz <vadim.grin28@gmail.com>
2023-02-06 21:42:30 +0200

    fix globalTheme

--- client/src/config/GlobalTheme.jsx
+++ client/src/config/GlobalTheme.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { blueGrey, orange, grey } from '@mui/material/colors'
-import darkMode from '../helpers/darkMode'
 
 function setTheme (isDark) {
   return createTheme({
@@ -60,64 +59,6 @@ function setTheme (isDark) {
     },
   })
 }
-
-const theme = createTheme({
-    palette: {
-      mode: darkMode ? 'dark': undefined,
-      primary: blueGrey,
-      info: darkMode ? grey : grey,
-      secondary: {
-        main: orange['A700']
-      }
-    },
-    typography: {
-      fontFamily: 'Sono',
-      fontSize: 25,
-      h1: {
-        fontSize: '4em',
-        fontWeight: 'bold',
-        fontVariant: 'all-petite-caps'
-      },
-      h2: {
-        fontSize: '3em',
-        fontVariant: 'all-petite-caps'
-      },
-      h3: {
-        fontSize: '2em',
-      },
-      h4: {
-        fontSize: '1.5em',
-      },
-      h4: {
-        fontSize: '1.2em',
-      },
-      h5: {
-        fontSize: '1.2em',
-      },
-      h6: {
-        fontSize: '0.8em',
-      },
-      body1: {
-        fontSize: '1.2em',
-      },
-      body2: {
-        fontSize: '1em',
-        color: 'white',
-      },
-      caption: {
-        fontSize: '1em',
-      },
-    },
-    components: {
-      // MuiTextField: {
-      //   styleOverrides: {
-      //     root: {
-      //       fontSize: '0.5rem',
-      //     },
-      //   },
-      // },
-    },
-  })
 
 function GlobalTheme({ children }) {
   const { isDarkTheme } = useSelector((state) => state.settings)
